3f152abe62020904aed7ee1e60c201edd72bb55e
Matheus Ribeiro <Nogahzera@gmail.com>
2023-05-09 15:04:37 -0300

    Initial commit

---
+++ src/main/java/com/curso/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.curso.springboot.thymeleafdemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.curso.springboot.thymeleafdemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// add method to sort by last name
+
+    public List<Employee> findAllByOrderByLastNameAsc();
+	
+}
